e2dd4c0785352604e61cc64263fce563c4b27661
Helloworld11224 <128186286+Helloworld11224@users.noreply.github.com>
2024-11-17 12:20:39 +0700

    first commit

---
+++ app.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const version = 2 + 1
+</script>
+
+<template>
+  <div class="hello">
+    Hello Nuxt {{ version }}!
+    <br>
+    Test123
+  </div>
+</template>
+
+<style scoped>
+.hello {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 3rem;
+  padding: 2rem;
+}
+</style>
+
